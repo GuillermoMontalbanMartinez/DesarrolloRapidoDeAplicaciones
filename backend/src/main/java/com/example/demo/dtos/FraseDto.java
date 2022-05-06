2779f66fb9f74c4203a3caf0d3e5961e41142b6b
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-06 12:44:36 +0200

    scrapping web personal terminado es mejorable

--- backend/src/main/java/com/example/demo/dtos/FraseDto.java
+++ backend/src/main/java/com/example/demo/dtos/FraseDto.java
@@ -2,19 +2,12 @@ package com.example.demo.dtos;
 
 public class FraseDto {
     public String frase;
-    public String autor;
 
     public FraseDto() {
     }
 
-    public FraseDto(String frase, String autor) {
-        this.frase = frase;
-        this.autor = autor;
-    }
-
     public FraseDto(String frase) {
         this.frase = frase;
-        this.autor = "anonimo";
     }
 
     public String getFrase() {
@@ -25,19 +18,10 @@ public class FraseDto {
         this.frase = frase;
     }
 
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
     @Override
     public String toString() {
         return "FraseDto{" +
                 "frase='" + frase + '\'' +
-                ", autor='" + autor + '\'' +
                 '}';
     }
 }
--- backend/src/main/java/com/example/demo/services/FraseService.java
+++ backend/src/main/java/com/example/demo/services/FraseService.java
@@ -1,18 +1,14 @@
 package com.example.demo.services;
 
 import com.example.demo.dtos.FraseDto;
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
+
+
 
 // Anotación del componente
 @Component("fraseService") // (1)
@@ -24,25 +20,22 @@ public class FraseService {
      * 3 => Selección de la etiqueta <blockquote> y se obtiene el texto de la frase y el autor de la frase (si existe)
      * @return Lista de frases
      */
-    public List<FraseDto> retrieveFrase() throws IOException {
-        List<FraseDto> fraseData = new ArrayList<>();
+    public List<FraseDto> retrieveFrase() {
+            List<FraseDto> frases = new ArrayList<>();
 
-        Playwright playwright = Playwright.create();
-        Browser browser = playwright.webkit().launch();
-        Page page = browser.newPage();
-        page.navigate("https://www.iebschool.com/blog/50-frases-motivadoras-para-emprendedores-creacion-empresas/");
-        page.waitForSelector("#post-24110 > div.content-body > div > div.entry-content > blockquote:nth-child(51)"); //(2) Esperamos que se cargue el selector último de la página
-        //Document webPage = Jsoup.connect("https://controlyperspectiva.com/2020/02/frases-estoicas-para-mejorar-tu-efectividad.html").get(); // (2)
-        // Element blockquote = webPage.getElementsByTag("blockquote").first(); // (3)
-        Document webPage = Jsoup.parse(page.content()); // (3)
-        Elements blockquote = webPage.getElementsByTag("blockquote"); // (3)
-        for (Element element : blockquote) {
-            String frase = element.text();
-            String autor = element.getElementsByTag("cite").text(); 
-            fraseData.add(new FraseDto(frase, autor));
-            
+            try {
+                Document webPage = Jsoup.connect("https://www.axahealthkeeper.com/blog/las-50-mejores-frases-para-motivarse-a-no-dejar-de-hacer-deporte/").get();
+                Element ol = webPage.getElementsByTag("ol").get(0);
+                
+                for (Element li : ol.getElementsByTag("li")) {
+                    frases.add(new FraseDto(li.text()));
+                }
+                return frases;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
         }
-        browser.close();
-        return fraseData;
+
     }
-}
