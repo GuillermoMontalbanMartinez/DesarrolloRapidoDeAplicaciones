68ebf2b56d46c062177d8cba51714300cdf40403
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-08 19:08:29 +0200

    consigo traerme ejercicios

--- frontend/src/app/body/body.component.ts
+++ frontend/src/app/body/body.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-body',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.scss']
 })
 export class BodyComponent implements OnInit {
+  exercises: any[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getExercise();
+  }
+
+  public async getExercise() {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+        'X-RapidAPI-Key': '6905b22d97msh0448444f3f9e16bp1a8733jsnd77925bf8e66'
+      }
+    };
+
+    for (let i = 28; i <= 32; i++) {
+    let url = `https://exercisedb.p.rapidapi.com/exercises/exercise/00` + i;
+    let rest = await fetch(url, options);
+    let exercise = await rest.json();
+    this.exercises.push(exercise);
+    }
+
   }
 
 }
--- frontend/src/app/exercise/exercise.component.ts
+++ frontend/src/app/exercise/exercise.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-exercise',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercise.component.scss']
 })
 export class ExerciseComponent implements OnInit {
-
+  @Input() name: string = "";
+  @Input() bodyPart: string = "";
+  @Input() target: string = "";
+  @Input() equipment: string = "";
+  
   constructor() { }
 
   ngOnInit(): void {
