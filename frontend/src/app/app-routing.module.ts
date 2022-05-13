9fb0ed273d41ea44967d521b584f58b5b74a5d00
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-13 15:35:00 +0200

    rutas con rutina

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
+import { RutineComponent } from './rutine/rutine.component';
 
 const routes: Routes = [
   { path: '', component: BodyComponent},
   { path: 'body', component: BodyComponent},
+  { path: 'rutine', component: RutineComponent},
   { path: '', redirectTo: '/ ', pathMatch: 'full'}
 ];
 
