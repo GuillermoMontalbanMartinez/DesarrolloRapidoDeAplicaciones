0460395ee730a9334b256a3bd4ddfe3b425ee873
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-15 18:56:01 +0200

    css al contenedor de los ejercicios cargados

--- frontend/src/app/exercise-upload/exercise-upload.component.ts
+++ frontend/src/app/exercise-upload/exercise-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharevaluesService } from '../sharevalues.service';
 
 @Component({
   selector: 'app-exercise-upload',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExerciseUploadComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shareValues: SharevaluesService) { }
 
   ngOnInit(): void {
   }
