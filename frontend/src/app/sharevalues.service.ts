e84ea054a7213d862050016a7d4701b72ea43bc6
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-15 18:21:57 +0200

    metodo para agregar valores al array a compartir y para devolver el array

--- frontend/src/app/sharevalues.service.ts
+++ frontend/src/app/sharevalues.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Iexercise } from './Iexercise';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharevaluesService {
+  private exerciseUpload:Iexercise[] = [];
 
   constructor() { }
+
+  addExerciseToArray(valor: any) {
+    const valueToBeAdded : Iexercise = {
+      bodyPart: valor.bodyPart,
+      equipment: valor.equipment,
+      gifUrl: valor.gifUrl,
+      id: valor.id,
+      name: valor.name,
+      target: valor.target
+    }
+    this.exerciseUpload.push(valueToBeAdded);
+  }
+
+  getExerciseUpload(): Iexercise[] {
+    return this.exerciseUpload;
+  }
+
+
 }
