257fd2ef1c86f71014fe7ffd3ae4f0bc52df7391
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-18 17:48:50 +0200

    cambios a los controllers

--- backend/src/main/java/com/example/demo/controllers/RutinasController.java
+++ backend/src/main/java/com/example/demo/controllers/RutinasController.java
@@ -6,14 +6,17 @@ import com.example.demo.entity.Routine;
 import com.example.demo.repository.RoutineRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/routines")
 public class RutinasController {
@@ -36,8 +39,8 @@ public class RutinasController {
     }
 
     @DeleteMapping(path = "")
-    public void deleteRutines(@RequestBody Routine routine) {
-        this.repository.delete(routine);
+    public void deleteRutines(@PathVariable long id) {
+        this.repository.deleteById(id);
     }
 
 }
