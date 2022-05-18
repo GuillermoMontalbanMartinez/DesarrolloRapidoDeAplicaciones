9ea383abcf195c999ba85438560692401add14e7
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-18 18:13:16 +0200

    refactoring

--- frontend/src/app/exercise-upload/exercise-upload.component.ts
+++ frontend/src/app/exercise-upload/exercise-upload.component.ts
@@ -6,25 +6,26 @@ import { SharevaluesService } from '../sharevalues.service';
 @Component({
   selector: 'app-exercise-upload',
   templateUrl: './exercise-upload.component.html',
-  styleUrls: ['./exercise-upload.component.scss']
+  styleUrls: ['./exercise-upload.component.scss'],
 })
 export class ExerciseUploadComponent implements OnInit {
-  @Input() name: string = "";
-  @Input() image: string = "";
-  @Input() bodyPart: string = "";
-  @Input() target: string = "";
-  @Input() equipment: string = "";
-  nameRoutine: string = "";
+  @Input() name: string = '';
+  @Input() image: string = '';
+  @Input() bodyPart: string = '';
+  @Input() target: string = '';
+  @Input() equipment: string = '';
+  nameRoutine: string = '';
 
-  Iroutine = { "name": '', "exercises": this.shareValues.getExerciseUpload() } as unknown as Iroutine;
+  Iroutine = {
+    name: '',
+    exercises: this.shareValues.getExerciseUpload(),
+  } as unknown as Iroutine;
 
-  constructor(public shareValues: SharevaluesService) { }
+  constructor(public shareValues: SharevaluesService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   saveRoutine(): void {
     this.Iroutine.name = this.nameRoutine;
   }
-
 }
--- frontend/src/app/tarjeta-exercise-upload/tarjeta-exercise-upload.component.ts
+++ frontend/src/app/tarjeta-exercise-upload/tarjeta-exercise-upload.component.ts
@@ -4,13 +4,11 @@ import { SharevaluesService } from '../sharevalues.service';
 @Component({
   selector: 'app-tarjeta-exercise-upload',
   templateUrl: './tarjeta-exercise-upload.component.html',
-  styleUrls: ['./tarjeta-exercise-upload.component.scss']
+  styleUrls: ['./tarjeta-exercise-upload.component.scss'],
 })
 export class TarjetaExerciseUploadComponent implements OnInit {
-  @Input() name: string = "";
-  constructor(public shareValues: SharevaluesService) { }
+  @Input() name: string = '';
+  constructor(public shareValues: SharevaluesService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
