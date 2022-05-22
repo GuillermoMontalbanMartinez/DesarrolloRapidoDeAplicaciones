37e90dc8ef5c748c3340152af8e6a07b1cf4395c
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-23 00:13:20 +0200

    frase componete falta poco

--- frontend/src/app/frase/frase.component.ts
+++ frontend/src/app/frase/frase.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FrasesService } from '../frases.service';
 
 @Component({
   selector: 'app-frase',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frase.component.scss']
 })
 export class FraseComponent implements OnInit {
+  frases:string[] = [];
+  frase: string = "";
 
-  constructor() { }
+  constructor(private frasesService: FrasesService) { }
 
   ngOnInit(): void {
+    this.getFrase()
   }
+
+  getFrase() {
+    this.frasesService.getFrases().subscribe(data => {
+      this.frases.push(data);
+    });
+    console.log(this.frases);
+    console.log(this.frases[this.random(0, this.frases.length - 1)]);
+    this.frase =  this.frases[this.random(0, this.frases.length - 1)];
+    console.log(this.frase);
+  }
+
+  random(min: number, max: number): number{
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
 
 }
