90ca2abb91c835a56397ac344a9ce4ce8ffacfb3
Guillermo Montalban Martinez <guillermosysti@gmail.com>
2022-05-24 00:27:32 +0200

    cambios en el scrapping ahora va dentro de las rutinas guardadas falta poner las tarjetas dentro de la rutina y mostrar

--- frontend/src/app/routines-saved/routines-saved.component.ts
+++ frontend/src/app/routines-saved/routines-saved.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-routines-saved',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./routines-saved.component.scss']
 })
 export class RoutinesSavedComponent implements OnInit {
-
+  @Input() nameRoutine: string = '';
   constructor() { }
 
   ngOnInit(): void {
